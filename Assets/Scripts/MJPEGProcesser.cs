e67565bf81804290a731ac156f651fc6d89a6c0b
Takayuki Sei <takayuki.sei@gree.net>
2019-11-13 23:51:14 +0900

    ...

---
+++ Assets/Scripts/MJPEGProcesser.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+public class MJPEGProcesser {
+    private readonly byte [] jpegHeader = new byte[] { 0xff, 0xd8 };
+    private int _chunkSize;
+    private bool _isStreamActive = false;
+    public byte [] CurrentFrame { get; private set; }
+    private SynchronizationContext _context;
+    private bool _responseReceived;
+    public event EventHandler<FrameReadyEventArgs> FrameReady;
+    public event EventHandler<ErrorEventArgs> Error;
+
+    public MJPEGProcesser(int chunkSize = 1024 * 4)
+    {
+        _context = SynchronizationContext.Current;
+        _chunkSize = chunkSize;
+    }
+
+
+    public void SetUri(Uri uri)
+    {
+        HttpWebRequest request = (HttpWebRequest) WebRequest.Create(uri);
+        request.BeginGetResponse(OnGetResponse, request);
+    }
+
+    private void OnGetResponse(IAsyncResult result)
+    {
+        _responseReceived = true;
+        byte [] imageBuffer = new byte[1024 * 1024];
+        HttpWebRequest request = (HttpWebRequest) result.AsyncState;
+
+        try
+        {
+            HttpWebResponse response = (HttpWebResponse) request.EndGetResponse(result);
+            string contentType = response.Headers["Content-Type"];
+            if (!string.IsNullOrEmpty(contentType) && !contentType.Contains("="))
+            {
+                throw new Exception("Invalid content-type header");
+            }
+
+            string boundary = contentType.Split('=')[1].Replace("\"", "");
+            byte[] boundaryBytes = Encoding
+                .UTF8
+                .GetBytes(boundary.StartsWith("--") ? boundary : "--" + boundary);
+            Stream stream = response.GetResponseStream();
+            BinaryReader reader = new BinaryReader(stream);
+
+            _isStreamActive = true;
+            byte[] buff = reader.ReadBytes(_chunkSize);
+            while (_isStreamActive)
+            {
+                int imageStart = FindBytes(buff, jpegHeader);
+                if (imageStart != -1)
+                {
+                    int size = buff.Length - imageStart;
+                    Array.Copy(buff, imageStart, imageBuffer, 0, size);
+
+                    while (true)
+                    {
+                        buff = reader.ReadBytes(_chunkSize);
+
+                        int imageEnd = FindBytes(buff, boundaryBytes);
+                        if (imageEnd != -1)
+                        {
+                            Array.Copy(buff, 0, imageBuffer, size, imageEnd);
+                            size += imageEnd;
+                            
+                            byte [] frame = new byte[size];
+                            Array.Copy(imageBuffer, 0, frame, 0, size);
+                            CurrentFrame = frame;
+
+                            if (FrameReady != null)
+                            {
+                               FrameReady(this, new FrameReadyEventArgs()); 
+                            }
+                            
+                            Array.Copy(buff, imageEnd, buff, 0, buff.Length - imageEnd);
+                            byte[] temp = reader.ReadBytes(imageEnd);
+                            Array.Copy(temp, 0, buff, buff.Length - imageEnd, temp.Length);
+                            break;
+                        }
+                        
+                        Array.Copy(buff, 0, imageBuffer, size, buff.Length);
+                        size += buff.Length;
+
+                        if (!_isStreamActive)
+                        {
+                            response.Close();
+                            break;
+                        }
+                    }
+                }
+            }
+            
+            response.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            if (Error != null)
+            {
+                _context.Post(delegate { Error(this, new ErrorEventArgs() { Message = e.Message});  }, null);
+            }
+        }
+    }
+
+// パフォーマンス？
+    private int FindBytes(byte[] buff, byte[] search)
+    {
+        for (int i = 0; i < buff.Length - search.Length; i++)
+        {
+            if (buff[i] == search[0])
+            {
+                int next;
+
+                for (next = 1; next < search.Length; next++)
+                {
+                    if (buff[i + next] != search[next])
+                    {
+                        break;
+                    }
+                }
+
+                if (next == search.Length)
+                {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
+
+
+public class FrameReadyEventArgs : EventArgs
+{
+  
+}
+
+public sealed class ErrorEventArgs : EventArgs
+
+{
+    public string Message { get; set; }
+    public int ErrorCode { get; set; }
+}
---
+++ Assets/Scripts/MJPEGTexture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MJPEGTexture : MonoBehaviour
+{
+	[SerializeField] private string Url;
+	private int _chunkSize = 4;
+	private Texture2D _texture;
+	private const int _initWidth = 2;
+	private const int _initHeight = 2;
+	private bool _updateFrame = false;
+	private MJPEGProcesser _processer;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_processer = new MJPEGProcesser(_chunkSize * 1024);
+		_processer.FrameReady += (obj, arg) => { _updateFrame = true; };
+		_processer.Error += (sender, e) => { Debug.Log("error"); };
+		Uri uri = new Uri(Url);
+		_texture = new Texture2D(_initWidth, _initHeight, TextureFormat.PVRTC_RGB4, false);
+		GetComponent<Renderer>().material.mainTexture = _texture;
+		
+		_processer.SetUri(uri);
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (_updateFrame)
+		{
+			_texture.LoadImage(_processer.CurrentFrame);
+		}
+	}
+}
